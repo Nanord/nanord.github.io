bca37ed5b0a58b2a916c3c534fbfdf9e9d6b01fb
Борисов <nanord2@yandex.ru>
2017-10-17 04:03:12 +0300

    Clear

--- search/all_5.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['nds',['NDS',['../mainwindow_8cpp.html#a06fc87d81c62e9abb8790b6e5713c55ba75a9bbe49b5990f3b94ff1ad1179b454',1,'mainwindow.cpp']]]
-];
--- search/functions_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['calc',['calc',['../classcalculate.html#adf67800c10da97b39f6f05ac75a1004a',1,'calculate']]],
-  ['calculate',['calculate',['../classcalculate.html#ae9ae84fe5b924e90d2eff41493319389',1,'calculate']]]
-];
